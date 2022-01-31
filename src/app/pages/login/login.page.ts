be378d0750dad34384ba5f0b3522837d3f386e68
FabriJuncal <juncalfabri@gmail.com>
2022-01-31 19:31:31 -0300

    Ionic Login | Fix/Estilos Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ActionSheetController } from '@ionic/angular';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -9,8 +11,15 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginPage implements OnInit {
 
+  public onLoginForm: FormGroup;
+
+  passwordType = 'password';
+  passwordIcon = 'eye-off';
+
   constructor(private authSvc: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder: FormBuilder,
+              private actionSheetCtrl: ActionSheetController) {}
 
   ngOnInit() {
     // Muestra los datos de la Sesión del Usuario en la consola
@@ -18,12 +27,22 @@ export class LoginPage implements OnInit {
     // .subscribe(user => {
     //   console.log('userdata->',user);
     // });
+
+    this.onLoginForm = this.formBuilder.group({
+      user: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+  public hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
   }
 
   // Logea al usuario con Email y Contraseña
-  async onLogin(email, password) {
+  async onLogin() {
     try {
-      const user = await this.authSvc.login(email.value, password.value);
+      const user = await this.authSvc.login(this.onLoginForm.value.user,this.onLoginForm.value.password);
       if(user){
         const isVerified = this.authSvc.isEmailVerified(user);
         this.redirectUser(isVerified);
@@ -46,6 +65,32 @@ export class LoginPage implements OnInit {
     }
   }
 
+  async onChangeImage(){
+
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: '¿Como desea cargar su imagen?',
+      buttons: [
+        {
+          text: 'Galería de imágenes',
+          handler: () => {
+            // this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Cámara de fotos',
+          handler: () => {
+            // this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
+
   // Verifica que el email del usuario se encuentre verificado y redirecciona
   private  redirectUser(isVerified: boolean): void {
     if(isVerified){
@@ -54,5 +99,9 @@ export class LoginPage implements OnInit {
       this.router.navigateByUrl('/verify-email');
     }
   }
+
+
+
+
 
 }
